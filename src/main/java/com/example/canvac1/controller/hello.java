43e9715514ca16bff5006fc20b9aa2f6f0586d06
rohaneth <rohanmuttepwar55@gmail.com>
2025-05-18 20:08:33 +0530

    Initial commit

--- src/main/java/com/example/canvac1/controller/hello.java
+++ src/main/java/com/example/canvac1/controller/hello.java
@@ -1,19 +1,13 @@
 package com.example.canvac1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/health")
-public class HealthCheckController {
-
+public class hello {
+    
     @GetMapping("/")
-    public String ping() {
-        return "pong";
-    }
-    @GetMapping("/ping")
     public String hello() {
-        return "pong";
+        return "Hello";
     }
 }
